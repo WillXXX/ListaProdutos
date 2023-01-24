fad5c743c5bc9094fc8c670407e81e6b27c8b787
Williams.Silva <https://github.com/WillXXX/BeberAgua.git>
2023-01-24 17:40:15 -0300

    Recyclerview + layoutManager

--- app/src/main/java/com/example/listaprodutos/MainActivity.kt
+++ app/src/main/java/com/example/listaprodutos/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.listaprodutos
 
+import android.icu.lang.UCharacter.VerticalOrientation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView_produtos = findViewById<RecyclerView>(R.id.rv_produtos)
+        recyclerView_produtos.layoutManager =   LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView_produtos.setHasFixedSize(true)
     }
 }
